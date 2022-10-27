f3b859771249a9df5465f5214b15954aa5c59386
Jónatas <116375043+JonatasAns@users.noreply.github.com>
2022-10-27 23:15:37 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,52 @@
+function showMessage1(img1) {
+    document.getElementById('img1').textContent = img1;
+}
+
+function showMessage2(img2) {
+    document.getElementById('img2').textContent = img2;
+}
+
+function showMessage3(img3) {
+    document.getElementById('img3').textContent = img3;
+}
+
+function showMessage4(img4) {
+    document.getElementById('img4').textContent = img4;
+}
+
+function showMessage5(img5) {
+    document.getElementById('img5').textContent = img5;
+}
+
+function showMessage6(img6) {
+    document.getElementById('img6').textContent = img6;
+}
+
+function showMessage7(img7) {
+    document.getElementById('img7').textContent = img7;
+}
+
+function showMessage8(img8) {
+    document.getElementById('img8').textContent = img8;
+}
+
+function showMessage9(img9) {
+    document.getElementById('img9').textContent = img9;
+}
+
+showMessage1("0 a 2 anos")
+showMessage2("2 a 12 anos")
+showMessage3("12 a 16 anos")
+showMessage4("18 a 18 anos")
+showMessage5("18 a 50 anos")
+showMessage6("50 a 65 anos")
+showMessage7("65 a 100 anos")
+showMessage8("100 a 130 anos")
+showMessage9("130 ano ou mais")
+
+if (personIs0to2years) {
+    document.getElementById('img1')
+}
+else{
+    document.getElementById('buttonarea').style.visibility = "hidden"
+}
